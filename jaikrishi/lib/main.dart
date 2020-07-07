600910fd504abc3e1870e5b6ca1e8988a321db4b
Siddhartha Mishra <siddhartham04@gmail.com>
2020-07-06 23:03:55 -0700

    provider

--- jaikrishi/lib/main.dart
+++ jaikrishi/lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (context) => UserModel(),
+    ),
+  ], child: MyApp()));
+}
+
+class UserModel extends ChangeNotifier {
+  String _phoneNumber;
+  String get phonNumber => _phoneNumber;
+  set phonNumber(String value) {
+    _phoneNumber = value;
+    notifyListeners();
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: App(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -21,8 +37,22 @@ class App extends StatefulWidget {
 
 class _App extends State<App> {
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
-    );
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            FirebaseUser user = snapshot.data;
+            if (user == null) {
+              return Container(
+                  child: Text("You have not created an account yet"));
+            } else {
+              return Container(child: Text("You have logged in already!"));
+            }
+          } else {
+            return Scaffold(
+              body: CircularProgressIndicator(),
+            );
+          }
+        });
   }
 }
