37acc5b8ce23a25399da174c11c53d51cfefc9ce
Gavin Hung <gavinkhung@gmail.com>
2020-07-14 23:02:50 -0700

    added firebase analytics

--- jaikrishi/lib/main.dart
+++ jaikrishi/lib/main.dart
@@ -6,8 +6,14 @@ import 'package:provider/provider.dart';
 import 'models/image_model.dart';
 import 'models/user_model.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 
 void main() {
+  final FirebaseAnalytics analytics = FirebaseAnalytics();
+  FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
+
   runApp(
     MultiProvider(
       providers: [
@@ -17,6 +23,8 @@ void main() {
         ChangeNotifierProvider(
           create: (context) => ImageModel(),
         ),
+        Provider<FirebaseAnalytics>.value(value: analytics),
+        Provider<FirebaseAnalyticsObserver>.value(value: observer),
       ],
       child: MyApp(),
     ),
@@ -38,6 +46,7 @@ class MyApp extends StatelessWidget {
         const Locale('hi', ''),
       ],
       home: App(),
+      navigatorObservers: [Provider.of<FirebaseAnalyticsObserver>(context)],
       debugShowCheckedModeBanner: false,
     );
   }
@@ -49,9 +58,21 @@ class App extends StatefulWidget {
   _App createState() => _App();
 }
 
+Future<void> _sendAnalyticsEvent(BuildContext context) async {
+  FirebaseAnalytics analytics = Provider.of<FirebaseAnalytics>(context);
+  await analytics.logEvent(name: 'started_app', parameters: <String, dynamic>{
+    'string': 'string',
+    'int': 42,
+    'long': 12345678910,
+    'double': 42.0,
+    'bool': true,
+  });
+}
+
 class _App extends State<App> {
   Widget build(BuildContext context) {
     print("hi");
+    _sendAnalyticsEvent(context);
     return Scaffold(
       body: autoLogin(context, Text("poo"), Upload(null, "")),
     );
