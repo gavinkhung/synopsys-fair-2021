ccfa69c2094a728db8b8e54ff2a62c58f3400350
Arnav Reddy <arnavreddy@gmail.com>
2021-02-19 23:20:01 -0800

    merge

--- jaikrishi/lib/screens/history/history.dart
+++ jaikrishi/lib/screens/history/history.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:leaf_problem_detection/models/user_model.dart';
